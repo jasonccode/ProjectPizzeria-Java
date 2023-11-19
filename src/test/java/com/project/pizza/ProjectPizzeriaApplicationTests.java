69239bac657bb61696bab56eb5aea3f769a0565e
Jason C <grousemusic@gmail.com>
2023-11-19 14:17:27 -0500

    Initial commit

---
+++ src/test/java/com/project/pizza/ProjectPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
