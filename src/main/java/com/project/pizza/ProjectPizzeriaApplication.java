ea81a361a74fdf840d703250028fa57e98af3162
Jason C <grousemusic@gmail.com>
2023-11-19 14:17:27 -0500

    Initial commit

---
+++ src/main/java/com/project/pizza/ProjectPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.project.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPizzeriaApplication.class, args);
+	}
+
+}
